71f8e60f51a183ead6aa8b2d47d492c520449257
Zorik Zakharyan <zakharyan.z.a@gmail.com>
2021-07-23 21:45:09 +0400

    First Push

---
+++ KeyBoard/KeyBoardDlg.h
@@ -0,0 +1,36 @@
+
+// KeyBoardDlg.h : header file
+//
+
+#pragma once
+
+
+// CKeyBoardDlg dialog
+class CKeyBoardDlg : public CDialogEx
+{
+// Construction
+public:
+	CKeyBoardDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_KEYBOARD_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CButton m_b1;
+};
---
+++ KeyBoard/resource.h
@@ -0,0 +1,79 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by KeyBoard.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_KEYBOARD_DIALOG             102
+#define IDR_MAINFRAME                   128
+#define IDC_BUTTON1                     1000
+#define IDC_BUTTON2                     1001
+#define IDC_BUTTON3                     1002
+#define IDC_BUTTON4                     1003
+#define IDC_BUTTON5                     1004
+#define IDC_BUTTON6                     1005
+#define IDC_BUTTON7                     1006
+#define IDC_BUTTON8                     1007
+#define IDC_BUTTON9                     1008
+#define IDC_BUTTON10                    1009
+#define IDC_BUTTON11                    1010
+#define IDC_BUTTON12                    1011
+#define IDC_BUTTON13                    1012
+#define IDC_BUTTON14                    1013
+#define IDC_BUTTON15                    1014
+#define IDC_BUTTON16                    1015
+#define IDC_BUTTON17                    1016
+#define IDC_BUTTON18                    1017
+#define IDC_BUTTON19                    1018
+#define IDC_BUTTON20                    1019
+#define IDC_BUTTON21                    1020
+#define IDC_BUTTON22                    1021
+#define IDC_BUTTON23                    1022
+#define IDC_BUTTON24                    1023
+#define IDC_BUTTON25                    1024
+#define IDC_BUTTON26                    1025
+#define IDC_BUTTON27                    1026
+#define IDC_BUTTON28                    1027
+#define IDC_BUTTON29                    1029
+#define IDC_BUTTON30                    1030
+#define IDC_BUTTON31                    1031
+#define IDC_BUTTON32                    1032
+#define IDC_BUTTON33                    1033
+#define IDC_BUTTON34                    1034
+#define IDC_BUTTON35                    1035
+#define IDC_BUTTON36                    1036
+#define IDC_BUTTON37                    1037
+#define IDC_BUTTON38                    1038
+#define IDC_BUTTON39                    1039
+#define IDC_BUTTON40                    1040
+#define IDC_BUTTON42                    1042
+#define IDC_BUTTON43                    1043
+#define IDC_BUTTON44                    1044
+#define IDC_BUTTON45                    1045
+#define IDC_BUTTON46                    1046
+#define IDC_BUTTON47                    1047
+#define IDC_BUTTON48                    1048
+#define IDC_BUTTON49                    1049
+#define IDC_BUTTON50                    1050
+#define IDC_BUTTON51                    1051
+#define IDC_BUTTON53                    1053
+#define IDC_BUTTON54                    1054
+#define IDC_BUTTON55                    1055
+#define IDC_BUTTON56                    1056
+#define IDC_BUTTON57                    1057
+#define IDC_BUTTON58                    1058
+#define IDC_BUTTON59                    1059
+#define IDC_BUTTON60                    1060
+#define IDC_BUTTON52                    1061
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
